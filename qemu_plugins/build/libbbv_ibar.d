2d51810a40d3a1d5c0f30d1701765fdf9392df82
Muneeb Ahmad <muneeb0ahmed3@gmail.com>
2026-02-13 14:15:54 -0800

    added back qemu_plugins source

--- qemu_plugins/build/libbbv_ibar.d
+++ qemu_plugins/build/libbbv_ibar.d
@@ -1,4 +1,4 @@
-build/libbbv_ibar.so: bbv_ibar.cc \
- /home/muneeb/qemu/include/qemu/qemu-plugin.h util.h
-/home/muneeb/qemu/include/qemu/qemu-plugin.h:
+build/libbbv_ibar.so: bbv_ibar.cc ../qemu/include/qemu/qemu-plugin.h \
+ util.h
+../qemu/include/qemu/qemu-plugin.h:
 util.h:
